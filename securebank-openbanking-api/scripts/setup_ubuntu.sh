a04796db5d39c633f9520b6b34a6a9bbfb40ac92
EduArn <eduarnofficial@gmail.com>
2026-08-24 21:30:55 +0530

    Update setup_ubuntu.sh

--- securebank-openbanking-api/scripts/setup_ubuntu.sh
+++ securebank-openbanking-api/scripts/setup_ubuntu.sh
@@ -91,3 +91,23 @@ echo "Then test:"
 echo ""
 echo "    docker ps"
 echo "    docker run hello-world"
+
+sudo apt update
+
+sudo apt install -y gnupg software-properties-common curl
+
+curl -fsSL https://apt.releases.hashicorp.com/gpg | \
+  sudo gpg --dearmor --yes \
+  -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) \
+signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com \
+$(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
+
+sudo apt update
+
+sudo apt install -y terraform
+
+terraform version
